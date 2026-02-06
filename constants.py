aa90849b961c71dec440b5eaffe23b2ee8acd0eb
Heavens001T <linf27818@gmail.com>
2026-02-06 13:17:37 +0100

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Fraud thresholds
+MAX_AMOUNT = 200_000
+MAX_TX_PER_HOUR = 5
+
+# Risk weights
+WEIGHT_AMOUNT = 0.4
+WEIGHT_TX_COUNT = 0.3
+WEIGHT_NEW_DEVICE = 0.2
+WEIGHT_LOCATION = 0.1
+
+# Transaction types
+TX_TYPES = ["Send Money", "Pay Bill", "Airtime Top-Up", "Withdraw Cash"]
+
+# User profiles
+USER_PROFILES = ["Normal", "VIP", "Agent"]
